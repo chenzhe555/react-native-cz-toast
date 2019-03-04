ce2fa100e6ea0c05b8f93976680a05ef1ea22fdc
chenzhe <chenzhe@meicai.cn>
2019-03-04 20:19:32 +0800

    添加原生Toast

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { NativeModules } from 'react-native';
 import CZPackElement from 'react-native-cz-pack-element';
 import Toast from './toast';
+const { RNCzToast } = NativeModules;
 
 
 /*
@@ -40,8 +42,34 @@ export default class CZToast extends Component {
 
         //赋值Toast新数组元素
         extraData['text'] = text;
-        extraData['during'] = (parseFloat(extraData['during'] ? extraData['during'] : 1.5))*1000;
+        extraData['during'] = parseFloat(extraData['during'] ? extraData['during'] : 1.5);
         extraData['img'] = img;
+        if (typeof extraData['showType'] == 'undefined') extraData['showType'] = 2;
+        extraData['native'] = 0;
+        CZToast.showArr.push(extraData);
+        //显示Toast
+        if (!CZToast.isShowToast) CZToast.showToast();
+    }
+
+    /*
+    * 显示原生Toast  (PS：暂时支持的是多次调用的话会依次显示,不会马上停止上次的)
+    * text: 显示的文本信息
+    * extraData: 额外参数:{
+    *   type: 显示类型：目前默认只有1，可不填
+    *   showType: 显示位置类型：1.底部 2.中间 3.顶部。默认是2
+    *   during: 显示间隔时间(秒,默认1.5秒)
+    *   textStyle: 文本视图样式：默认：{marginLeft: 16, marginRight: 16, marginBottom: 15, marginTop: 15}
+    * }
+    * */
+    static showNative = (text = '', extraData = {}, img = null) => {
+        if (text.length <= 0) return;
+
+        //赋值Toast新数组元素
+        extraData['text'] = text;
+        extraData['during'] = parseFloat(extraData['during'] ? extraData['during'] : 1.5);
+        extraData['img'] = img;
+        if (typeof extraData['showType'] == 'undefined') extraData['showType'] = 2;
+        extraData['native'] = 1;
         CZToast.showArr.push(extraData);
         //显示Toast
         if (!CZToast.isShowToast) CZToast.showToast();
@@ -51,18 +79,34 @@ export default class CZToast extends Component {
     static showToast = () => {
         //当前显示数组第一个Toast对象
         let item = CZToast.showArr[0];
-        let element = new CZPackElement(<Toast data={item}/>);
         CZToast.isShowToast = true;
-        //during时间后再做逻辑判断
-        setTimeout( () => {
-            element.destoryElement();
-            CZToast.showArr.splice(0,1);
-            if (CZToast.showArr.length > 0) {
-                CZToast.showToast();
-            } else {
-                CZToast.isShowToast = false;
-            }
-        }, item['during']);
+
+        if (item['native'] == 0) {
+            //RN
+            let element = new CZPackElement(<Toast data={item}/>);
+            //during时间后再做逻辑判断
+            setTimeout( () => {
+                element.destoryElement();
+                CZToast.showArr.splice(0,1);
+                if (CZToast.showArr.length > 0) {
+                    CZToast.showToast();
+                } else {
+                    CZToast.isShowToast = false;
+                }
+            }, item['during']*1000);
+        } else {
+            //Native
+            RNCzToast.showToastWithType(item['showType'] == 2 ? 1001 : 1002, item['text'], item['during']);
+            //during时间后再做逻辑判断
+            setTimeout( () => {
+                CZToast.showArr.splice(0,1);
+                if (CZToast.showArr.length > 0) {
+                    CZToast.showToast();
+                } else {
+                    CZToast.isShowToast = false;
+                }
+            }, item['during']*1000);
+        }
     }
 
     /************************** 子组件回调方法 **************************/
--- ios/RNCzToast.m
+++ ios/RNCzToast.m
@@ -9,7 +9,7 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
-RCT_EXPORT_METHOD(showToastWithType:(NSInteger)type text:(NSString *)text time:(CGFloat):time)
+RCT_EXPORT_METHOD(showToastWithType:(NSInteger)type text:(NSString *)text time:(CGFloat)time)
 {
   dispatch_async(dispatch_get_main_queue(), ^{
     [[HHZToastView shareManager] showToastType:type text:text time:time];
