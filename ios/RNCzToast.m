779ffec2b04383f291f7fd34486ba4a1458c8695
chenzhe <chenzhe@meicai.cn>
2019-03-04 18:37:28 +0800

    更新 podspec

--- ios/RNCzToast.m
+++ ios/RNCzToast.m
@@ -1,5 +1,6 @@
 
 #import "RNCzToast.h"
+#import <HHZAlert/HHZToastView.h>
 
 @implementation RNCzToast
 
@@ -8,6 +9,11 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
-
+RCT_EXPORT_METHOD(showToastWithType:(NSInteger)type text:(NSString *)text time:(CGFloat):time)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [[HHZToastView shareManager] showToastType:type text:text time:time];
+  });
+}
 @end
   
